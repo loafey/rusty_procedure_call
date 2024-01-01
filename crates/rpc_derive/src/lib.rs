f0d9604021034a3e903da4d00771646748de03e7
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2024-01-01 18:58:31 +0100

    Did some deduplication.

--- crates/rpc_derive/src/lib.rs
+++ crates/rpc_derive/src/lib.rs
@@ -1,12 +1,64 @@
-extern crate proc_macro;
 use non_persistent::non_persistent;
 use persistent::persistent;
 use proc_macro::TokenStream as TS;
-use proc_macro2::TokenStream;
-use syn::ItemImpl;
+use proc_macro2::{Ident, TokenStream};
+use syn::{
+    punctuated::Punctuated, token::Comma, GenericParam, ImplItem, ItemImpl, PatType, ReturnType,
+    Type, Visibility,
+};
 
 mod non_persistent;
 mod persistent;
+
+fn get_generics(_ty: &PatType) -> Punctuated<GenericParam, Comma> {
+    Punctuated::new()
+}
+
+type Function = (Ident, Punctuated<PatType, Comma>, Option<Type>);
+struct FunctionTypes {
+    _generics: Punctuated<GenericParam, Comma>,
+    functions: Vec<Function>,
+}
+fn get_function_types(items: Vec<ImplItem>) -> FunctionTypes {
+    let mut _generics = Punctuated::new();
+    let mut functions = Vec::new();
+    for item in items {
+        if let ImplItem::Fn(func) = item {
+            if !matches!(func.vis, Visibility::Public(..)) {
+                continue;
+            }
+            let func_name = func.sig.ident;
+            let mut args: Punctuated<_, Comma> = Punctuated::new();
+            let mut is_self_ref = false;
+            for arg in func.sig.inputs.into_iter() {
+                match arg {
+                    syn::FnArg::Receiver(r) => {
+                        is_self_ref = r.reference.is_some();
+                    }
+                    syn::FnArg::Typed(t) => {
+                        _generics.extend(get_generics(&t));
+                        args.push(t);
+                    }
+                }
+            }
+            let ret_type = match func.sig.output {
+                ReturnType::Default => None,
+                ReturnType::Type(_, t) => Some(*t),
+            };
+            if is_self_ref {
+                functions.push((func_name, args, ret_type));
+            }
+        }
+    }
+    FunctionTypes {
+        _generics,
+        functions,
+    }
+}
+
+fn create_ident(s: &str) -> Ident {
+    syn::Ident::new(s, proc_macro2::Span::call_site())
+}
 
 #[derive(Debug, Clone, Copy, Default)]
 enum Attr {
--- crates/rpc_derive/src/non_persistent.rs
+++ crates/rpc_derive/src/non_persistent.rs
@@ -1,61 +1,8 @@
-extern crate proc_macro;
+use crate::{create_ident, get_function_types, FunctionTypes};
 use proc_macro::TokenStream as TS;
-use proc_macro2::{Ident, TokenStream};
+use proc_macro2::TokenStream;
 use quote::{quote, ToTokens};
-use syn::{
-    punctuated::Punctuated, token::Comma, GenericParam, ImplItem, ItemImpl, Pat, PatType,
-    ReturnType, Type, Visibility,
-};
-
-fn get_generics(_ty: &PatType) -> Punctuated<GenericParam, Comma> {
-    Punctuated::new()
-}
-
-type Function = (Ident, Punctuated<PatType, Comma>, Option<Type>);
-struct FunctionTypes {
-    _generics: Punctuated<GenericParam, Comma>,
-    functions: Vec<Function>,
-}
-fn get_function_types(items: Vec<ImplItem>) -> FunctionTypes {
-    let mut _generics = Punctuated::new();
-    let mut functions = Vec::new();
-    for item in items {
-        if let ImplItem::Fn(func) = item {
-            if !matches!(func.vis, Visibility::Public(..)) {
-                continue;
-            }
-            let func_name = func.sig.ident;
-            let mut args: Punctuated<_, Comma> = Punctuated::new();
-            let mut is_self_ref = false;
-            for arg in func.sig.inputs.into_iter() {
-                match arg {
-                    syn::FnArg::Receiver(r) => {
-                        is_self_ref = r.reference.is_some();
-                    }
-                    syn::FnArg::Typed(t) => {
-                        _generics.extend(get_generics(&t));
-                        args.push(t);
-                    }
-                }
-            }
-            let ret_type = match func.sig.output {
-                ReturnType::Default => None,
-                ReturnType::Type(_, t) => Some(*t),
-            };
-            if is_self_ref {
-                functions.push((func_name, args, ret_type));
-            }
-        }
-    }
-    FunctionTypes {
-        _generics,
-        functions,
-    }
-}
-
-fn create_ident(s: &str) -> Ident {
-    syn::Ident::new(s, proc_macro2::Span::call_site())
-}
+use syn::{punctuated::Punctuated, token::Comma, ItemImpl, Pat};
 
 pub fn non_persistent(org: TokenStream, nodes: ItemImpl) -> TS {
     let mut arg_enum = quote!();
