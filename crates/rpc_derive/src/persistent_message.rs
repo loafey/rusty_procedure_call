c94e7a7878674c1f72ecb47b00982b37b6fcc7d1
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2024-01-05 16:20:48 +0100

    Major revamp using message-io

--- crates/rpc_derive/src/persistent_message.rs
+++ crates/rpc_derive/src/persistent_message.rs
@@ -8,18 +8,128 @@ pub fn persistent(org: TokenStream, nodes: ItemImpl) -> TS {
     let this_type = nodes.self_ty;
 
     let server_name = create_ident(&format!("{}Server", this_type.to_token_stream()));
+    let arg_name = create_ident(&format!("__{}RpcArg", this_type.to_token_stream()));
     let client_name = create_ident(&format!("{}Client", this_type.to_token_stream()));
+    let client_connection_name =
+        create_ident(&format!("{}Connection", this_type.to_token_stream()));
+    let mut arg_enum = quote!();
+    let mut serve_match = quote!();
+    let mut new_impl = quote!();
+
+    let FunctionTypes {
+        _generics,
+        functions,
+    }: FunctionTypes = get_function_types(nodes.items);
+
+    for (i, t, r) in functions {
+        let ret_string = if let Some(ret) = &r {
+            quote!(#ret)
+        } else {
+            quote!(())
+        };
+        let args_without_types = t
+            .clone()
+            .into_iter()
+            .filter_map(|p| {
+                if let Pat::Ident(i) = *p.pat {
+                    Some(i.ident)
+                } else {
+                    None
+                }
+            })
+            .collect::<Punctuated<_, Comma>>();
+        new_impl = {
+            let args = if args_without_types.is_empty() {
+                quote!()
+            } else {
+                quote!((#args_without_types))
+            };
+
+            quote! {
+                #new_impl
+                // TODO should respect mutability here!
+                pub async fn #i (&mut self, #t ) -> Result< #ret_string , crate::RpcError > {
+                    use tokio::net::TcpStream;
+                    use tokio::io::{ AsyncWriteExt, AsyncReadExt };
+
+                    let stream = &mut self.stream;
+
+                    let value = ::rusty_procedure_call::postcard::to_allocvec(& #arg_name :: #i #args)?;
+                    let len = value.len() as u64;
+
+                    //println!("Writing message of len {len}");
+
+                    stream.write_u64(len).await?;
+                    stream.write_all(&value[..]).await?;
+
+                    // Bottleneck here!
+                    let len = stream.read_u64().await? as usize;
+                    let mut buf = vec![0; len];
+                    stream.read_exact(&mut buf).await?;
+
+
+                    let res = ::rusty_procedure_call::postcard :: from_bytes :: < #ret_string >(&buf[..])?;
+
+                    Ok(res)
+                }
+            }
+        };
+
+        let res_call = if !args_without_types.is_empty() {
+            quote!(
+                let res = self. #i (#args_without_types);
+            )
+        } else {
+            quote!(
+                let res = self.#i();
+            )
+        };
+
+        let m = if !args_without_types.is_empty() {
+            quote!(#arg_name :: #i (#args_without_types))
+        } else {
+            quote!(#arg_name :: #i)
+        };
+
+        serve_match = quote! {
+            #serve_match
+            #m => {
+                #res_call
+                let bytes = ::rusty_procedure_call::postcard::to_allocvec(&res)?;
+                let mut stream = self.__client_channels.get_mut(&id).ok_or(::rusty_procedure_call::RpcError::MissingClient)?;
+                stream.send(bytes).await?;
+            },
+        };
+
+        if !t.is_empty() {
+            let t = t
+                .into_iter()
+                .map(|t| t.ty)
+                .collect::<Punctuated<_, Comma>>();
+            arg_enum = quote! {
+                #arg_enum
+                #i (#t),
+            };
+        } else {
+            arg_enum = quote! {
+                #arg_enum
+                #i,
+            };
+        }
+    }
 
     let server_struct = quote! {
         pub struct #server_name {
             port: u16,
-            inner: #this_type
+            inner: #this_type,
+            connections: std::collections::HashMap<u64, #client_connection_name>,
         }
         impl #server_name {
             pub fn new(inner: #this_type, port: u16) -> Self {
                 Self {
                     port,
-                    inner
+                    inner,
+                    connections: std::collections::HashMap::new()
                 }
             }
             pub fn serve(self) -> Result<std::thread::JoinHandle<()>, ::rusty_procedure_call::RpcError> {
@@ -36,9 +146,11 @@ pub fn persistent(org: TokenStream, nodes: ItemImpl) -> TS {
                 let t = std::thread::spawn(move || {
                     listener.for_each(move |event| match event.network() {
                         NetEvent::Connected(_, _) => unreachable!(), // Used for explicit connections.
-                        NetEvent::Accepted(_endpoint, _listener) => println!("Client connected"), // Tcp or Ws
+                        NetEvent::Accepted(endpoint, _listener) => {
+                            // self.connections.insert(endpoint, listener);
+                        }, // Tcp or Ws
                         NetEvent::Message(endpoint, data) => {
-                            println!("Server - Received: {}", String::from_utf8_lossy(data));
+                            println!("Server - Received: {} {endpoint:?}", String::from_utf8_lossy(data));
                             handler.network().send(endpoint, data);
                         }
                         NetEvent::Disconnected(_endpoint) => println!("Client disconnected"), //Tcp or Ws
@@ -47,6 +159,13 @@ pub fn persistent(org: TokenStream, nodes: ItemImpl) -> TS {
 
                 Ok(t)
             }
+        }
+    };
+
+    let client_connection = quote! {
+        pub struct #client_connection_name {
+            udp: message_io::network::Endpoint,
+            tcp: message_io::network::Endpoint
         }
     };
 
@@ -65,9 +184,18 @@ pub fn persistent(org: TokenStream, nodes: ItemImpl) -> TS {
             }
         }
     };
+    let arg_enum = quote! {
+        #[allow(non_camel_case_types)]
+        #[derive(serde::Deserialize,serde::Serialize, Debug)]
+        enum #arg_name {
+            #arg_enum
+        }
+    };
 
     quote! {
         #org
+        #arg_enum
+        #client_connection
         #server_struct
         #client_struct
     }
